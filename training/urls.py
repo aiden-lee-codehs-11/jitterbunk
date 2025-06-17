8a11e7832844190a5fed37ea5c6dcd92f6b0a51d
Aiden Lee <alee@codehs.com>
2025-06-17 11:07:07 -0700

    style form and add thank you page

--- training/urls.py
+++ training/urls.py
@@ -7,6 +7,7 @@ app_name = 'training'
 
 urlpatterns = [
     path('', views.ResourceView.as_view(), name="index"),
+    path('thanks/', views.thanks_view, name='thanks_page'),
     path('<int:pk>/', views.UserView.as_view(), name='personal'),
     path('<str:team>/', views.TeamView.as_view(), name='team'),
     path('<int:user_id>/add/', views.add, name="add"),
